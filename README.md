2cf4abc57b3447eb6b54c4c8a4c270bb2639a341
Marco Berri <marcoberri@gmail.com>
2018-08-07 23:53:05 +0200

    First import project

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Starng
+# SwapiNG
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.8.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,50 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
+
+
+// Prime ng
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+import { DialogModule } from 'primeng/dialog';
+
+// Componeti del mio progetto
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ItemListComponent } from './components/item-list/item-list.component';
+import { ItemDetailsComponent } from './components/item-details/item-details.component';
+
+// Principale
 import { AppComponent } from './app.component';
+
+
+// Routing
+import { routingModule } from './app.routing';
+import { ItemListPeopleComponent } from './components/item-list-people/item-list-people.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ItemListComponent,
+    ItemListPeopleComponent,
+    ItemDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routingModule,
+    HttpClientModule,
+
+    // PrimeNG
+    ButtonModule,
+    TableModule,
+    DialogModule,
+
+
   ],
-  providers: [],
+  providers: [
+
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
