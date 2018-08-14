c1fdce5dd567fb1ab92799825b175812d27a6084
Marco Berri <marco.berri@sincositalia.it>
2018-08-14 10:21:19 +0200

    - creazione documentazione con compodoc - inizio ottimizzazione classi

---
+++ src/app/components/item-list-base.ts
@@ -0,0 +1,34 @@
+export class ItemListBase {
+  public totalRecords: number;
+  public loading = true;
+  public currentPage = 1;
+
+  public customFields;
+
+  public modalItem;
+  public displayModalDetail = false;
+  public modalType: string;
+
+  /**
+   *Reseta la modal sull'evento di chiusura dello stesso
+   *
+   * @param {Event} Event
+   */
+  public resetModal(Event: Event) {
+    this.displayModalDetail = false;
+    this.modalItem = null;
+    this.modalType = null;
+  }
+
+  /**
+   * Visualizza la modale con le info dell'object richiesto
+   *
+   * @param {*} item
+   * @param {string} type
+   */
+  public displayModalPannel(item: any, type: string) {
+    this.displayModalDetail = true;
+    this.modalItem = item;
+    this.modalType = type;
+  }
+}
